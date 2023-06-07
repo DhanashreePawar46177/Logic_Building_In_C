57e77ca37ed0a161e1f71e34ae3f28229531465c
DhanashreePawar46177 <DhanashreePawar46177@gmail.com>
2023-06-07 13:20:20 +0530

    Assignment

---
+++ Logic_Building_Assignment_01/02.Program To Print 5 Times Marvellous On Screen.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display()
+{
+    int i = 0;
+
+    for(i = 1; i <= 5; i++)
+    {
+        printf("\n Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    getch();
+    return 0;
+}
+
