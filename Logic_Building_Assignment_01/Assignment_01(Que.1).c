942db394f6e803dbc076acd9118152121cb6050b
DhanashreePawar46177 <DhanashreePawar46177@gmail.com>
2023-06-08 19:57:27 +0530

    Assignment

---
+++ Logic_Building_Assignment_01/Assignment_01(Que.1).c
@@ -0,0 +1,30 @@
+///01.Program To Divide Two Numbers.
+
+#include<stdio.h>
+#include<conio.h>
+
+int Divide(int iNo1,int iNo2)
+{
+    int iAns = 0;
+
+    if(iNo2 > iNo1)
+    {
+        return -1;
+    }
+
+    iAns = iNo1/iNo2;
+    return iAns;
+}
+
+int main()
+{
+    int iValue1 = 15, iValue2 = 5;
+    int iRet = 0;
+
+    iRet = Divide(iValue1,iValue2);
+
+    printf("\n Division is %d.\n",iRet);
+
+    getch();
+    return 0;
+}
